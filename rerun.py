dbe3cad047e8eeca87d125f35d7506d52b31d917
Jonathan Wright <jonathan@quag.geek.nz>
2009-06-11 18:44:14 +1200

    Adding a python rerun script

---
+++ rerun.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import time
+import shutil
+import difflib
+
+def printDiff(file1, file2):
+    f1 = file(file1)
+    f2 = file(file2)
+
+    print "".join(difflib.unified_diff(f1.readlines(), f2.readlines()))
+
+    f1.close()
+    f2.close()
+
+script = sys.argv[1]
+scriptbackup = script + "~"
+
+pid = None
+lastrun = 0
+
+while True:
+    mtime = os.stat(script).st_mtime
+
+    if mtime > lastrun:
+        if pid:
+            os.kill(pid, 15) # SIGTERM: 15
+            printDiff(scriptbackup, script)
+
+        shutil.copyfile(script, scriptbackup)
+
+        pid = os.spawnl(os.P_NOWAIT, script)
+        lastrun = mtime
+
+        print "###", script, "started ###"
+
+    time.sleep(0.5)
---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+def heh():
+    print "heh"
+
+for i in range(1, 3):
+    heh()
+
