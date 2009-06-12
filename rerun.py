c01d1cda4116b15330c370d3ba36bc8e751307de
Jonathan Wright <quaggy@gmail.com>
2009-06-12 20:48:53 +1200

    Putting script inside a __name__ == "__main__"

--- rerun.py
+++ rerun.py
@@ -15,25 +15,26 @@ def printDiff(file1, file2):
     f1.close()
     f2.close()
 
-script = sys.argv[1]
-scriptbackup = script + "~"
+if __name__ == "__main__":
+    script = sys.argv[1]
+    scriptbackup = script + "~"
 
-pid = None
-lastrun = 0
+    pid = None
+    lastrun = 0
 
-while True:
-    mtime = os.stat(script).st_mtime
+    while True:
+        mtime = os.stat(script).st_mtime
 
-    if mtime > lastrun:
-        if pid:
-            os.kill(pid, 15) # SIGTERM: 15
-            printDiff(scriptbackup, script)
+        if mtime > lastrun:
+            if pid:
+                os.kill(pid, 15) # SIGTERM: 15
+                printDiff(scriptbackup, script)
 
-        shutil.copyfile(script, scriptbackup)
+            shutil.copyfile(script, scriptbackup)
 
-        pid = os.spawnl(os.P_NOWAIT, script)
-        lastrun = mtime
+            pid = os.spawnl(os.P_NOWAIT, script)
+            lastrun = mtime
 
-        print "###", script, "started ###"
+            print "###", script, "started ###"
 
-    time.sleep(0.5)
+        time.sleep(0.5)
