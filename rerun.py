2d496b7b58030dd0e98c063c539dd0336100f9bd
Jonathan Wright <quaggy@gmail.com>
2009-06-13 11:04:18 +1200

    Moved the __name__ == "__main__" code into a function

--- rerun.py
+++ rerun.py
@@ -7,25 +7,24 @@ import difflib
 import signal
 import subprocess
 
-def kill(process):
+def _kill(process):
     try:
         process.terminate()
     except:
         # Fall back for pre-2.6, but only works on Unix platforms
         os.kill(process.pid, signal.SIGTERM)
 
-def readlines(path):
+def _readlines(path):
     f = file(path)
     lines = f.readlines()
     f.close
     return lines
 
-def snapshot(name, timestamp):
+def _snapshot(name, timestamp):
     return "%s (%s)" % (name, time.asctime(time.localtime(timestamp)))
 
-if __name__ == "__main__":
+def rerun(script):
     try:
-        script = sys.argv[1]
         scriptpath = os.path.abspath(script)
 
         process = None
@@ -38,22 +37,25 @@ if __name__ == "__main__":
             if lastmodified > lastrun:
 
                 if process:
-                    kill(process)
+                    _kill(process)
 
-                contents = readlines(script)
+                contents = _readlines(script)
 
                 if lastcontents:
                     print
-                    print "".join(difflib.unified_diff(lastcontents, contents, fromfile=snapshot(script, lastrun), tofile=snapshot(script, lastmodified)))
+                    print "".join(difflib.unified_diff(lastcontents, contents, fromfile=_snapshot(script, lastrun), tofile=_snapshot(script, lastmodified)))
 
                 lastcontents = contents
 
                 process = subprocess.Popen(scriptpath)
 
-                print "###", snapshot(script, lastmodified), "started ###"
+                print "###", _snapshot(script, lastmodified), "started ###"
 
                 lastrun = lastmodified
 
             time.sleep(0.5)
     except KeyboardInterrupt:
         print
+
+if __name__ == "__main__":
+    rerun(sys.argv[1])
